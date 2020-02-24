b10b1479000a783b7fa329e0ea89ad35f33d5b96
Tadeu do Nascimento <tadeunascimento36@gmail.com>
2020-02-24 18:51:13 -0300

    Estruturas básicas de um programa em PL/SQL.

---
+++ Estudo/estruturas_programas.sql
@@ -0,0 +1,82 @@
+--Estrutura do bloco de um programa em PL/SQL
+DECLARE
+--Nesta seção são declaradas constantes e variáveis e cursores.
+BEGIN
+--Nesta seção apresenta comandos SQL, estruturas de programação
+--e blocos aninhados.
+
+EXCEPTION
+--Nesta seção são realizados os tratamentos de excessões e emissões
+--de mensagens.
+END;
+--Indica o fim do programa.
+
+--Exemplo de programa PL/SQL
+SET SERVEROUTPUT ON
+DECLARE
+V_MENSAGEM VARCHAR(50):='Olá Mundo PL/SQL';
+BEGIN
+DBMS_OUTPUT.PUT_LINE(V_MENSAGEM);
+END;
+
+--Exemplo de blocos aninhados.
+DECLARE
+--.....
+BEGIN
+--.....
+BEGIN
+--.....
+EXCEPTION
+--.....
+END;
+--.....
+EXCEPTION
+--.....
+END;
+
+--Estrutura básica de uma procedure.
+CREATE OR REPLACE PROCEDURE NOME_PROCEDURE
+(ARGUMENTO_1 MODO TIPO_DADO,
+....
+ARGUMENTO_N MODO TIPO_DADO)IS
+NOME_VARIAVEL_LOCAL_1 TIPO_DADO;
+....
+NOME_VARIAVEL_LOCAL_N TIPO_DADO;
+
+BEGIN
+.....
+END;
+
+--Chamando uma procedure.
+EXECUTE NOME_PROCEDURE(ARGUMENTO_1,...ARGUMENTO_N);
+
+--Estrutura básica de uma Function(Função).
+CREATE OR REPLACE FUNCTION NOME_FUNCAO
+(ARGUMENTO_1 MODO TIPO_DADO,
+....
+ARGUMENTO_N MODO TIPO_DADO)
+RETURN TIPO_DADO IS NOME_VARIAVEL_LOCAL_1 TIPO_DADO;
+....
+NOME_VARIAVEL_LOCALN, TIPO_DADO;
+BEGIN
+....
+END;
+
+--Chamando uma function.
+SELECT NOME_FUNCTION(ARGUMENTO_1,....ARGUMENTO_N)FROM DUAL;
+--DUAL é o nome de uma pseudo-tabela utilizado para que o
+--comando SELECT possa atender à sintaxe padrão da linguagem SQL.
+
+-- Triggers
+-- Sintaxe básica de um trigger(Gatilho):
+CREATE OR REPLACE TRIGGER NOME_TRIGGER
+BEFORE|AFTER INSERT|UPDATE OF NOME_COLUNA ON NOME_TABELA
+FOR EACH ROW
+BEGIN
+...
+END NOME_TRIGGER
+
+--A instrução SELECT recupera os dados do banco para as variáveis
+--PL/SQL. A sintaxe básica para isso é apresentada a seguir:
+SELECT NOME_DA_COLUNA INTO NOME_DA_VARIAVEL
+FROM NOME_DA_TABELA WHERE ...; 
