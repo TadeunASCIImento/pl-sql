0aa884afa298999f978f42426f7fd9882b3ec37e
Tadeu do Nascimento <tadeunascimento36@gmail.com>
2020-04-28 20:58:21 -0300

    Recuperando o cargo do funcionário da tabela Jobs com INNER JOIN.

--- Estudo/exercicio_hr_4.sql
+++ Estudo/exercicio_hr_4.sql
@@ -17,4 +17,26 @@ SET SERVEROUTPUT ON
                 EXIT WHEN crs_emp%ROWCOUNT>=10 OR crs_emp%NOTFOUND;
                     END LOOP;
                     CLOSE crs_emp;
-                END;
+                    END;
+
+-- Realizando Inner Join com a tabela Jobs para recuperar o cargo do funcionário.
+--
+SET SERVEROUTPUT ON
+        DECLARE
+        CURSOR crs_emp IS
+        SELECT first_name,last_name,salary,job_title FROM employees
+        INNER JOIN jobs ON jobs.job_id = employees.job_id;
+                   vrl_first_name employees.first_name%TYPE;
+                   vrl_last_name employees.last_name%TYPE;
+                   vrl_salary employees.salary%TYPE;
+                   vrl_cargo jobs.job_title%TYPE;
+         BEGIN
+              OPEN crs_emp;
+              LOOP
+                FETCH crs_emp INTO vrl_first_name,vrl_last_name,vrl_salary,vrl_cargo;
+                      dbms_output.put_line(' Nome: '||vrl_first_name||
+                     ' Sobrenome: '||vrl_last_name||' Salário: '||vrl_salary||' Cargo: '||vrl_cargo);
+                EXIT WHEN crs_emp%ROWCOUNT>=10 OR crs_emp%NOTFOUND;
+                     END LOOP;
+                     CLOSE crs_emp;
+                     END;
